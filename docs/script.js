0488fd39eae9b7f1f20e02ba57c72d2c3ce5bb9e
yeemachine <richtomyee@gmail.com>
2021-12-09 00:31:33 -0800

    change import to index

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,4 @@
-import * as Kalidokit from "../dist/kalidokit.es";
+import * as Kalidokit from "../dist";
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
