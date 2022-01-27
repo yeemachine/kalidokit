98b97394787448d390ddb65d577e970d76394151
Philipp Niederlag <philipp.niederlag@artcom.de>
2022-01-27 22:01:17 +0100

    Improve arm rotation

--- src/PoseSolver/calcArms.ts
+++ src/PoseSolver/calcArms.ts
@@ -1,4 +1,5 @@
 import Vector from "../utils/vector";
+import Euler from "../utils/euler";
 import { clamp } from "../utils/helpers";
 import { Results, Side } from "../Types";
 import { RIGHT, LEFT } from "./../constants";
@@ -10,30 +11,57 @@ import { PI } from "./../constants";
  */
 export const calcArms = (lm: Results) => {
     //Pure Rotation Calculations
-    const UpperArm = {
-        r: Vector.findRotation(lm[11], lm[13]),
-        l: Vector.findRotation(lm[12], lm[14]),
+    const rightUpperArmSphericalCoords = Vector.getRelativeSphericalCoords(lm[12], lm[11], lm[13], {
+        x: "z",
+        y: "x",
+        z: "y",
+    });
+    const leftUpperArmSphericalCoords = Vector.getRelativeSphericalCoords(lm[11], lm[12], lm[14], {
+        x: "z",
+        y: "x",
+        z: "y",
+    });
+
+    const rightLowerArmSphericalCoords = Vector.getRelativeSphericalCoords(lm[11], lm[13], lm[15], {
+        x: "z",
+        y: "x",
+        z: "y",
+    });
+    const leftLowerArmSphericalCoords = Vector.getRelativeSphericalCoords(lm[12], lm[14], lm[16], {
+        x: "z",
+        y: "x",
+        z: "y",
+    });
+
+    const Hand = {
+        r: Vector.findRotation(new Vector(lm[15]), Vector.lerp(new Vector(lm[17]), new Vector(lm[19]), 0.5)),
+        l: Vector.findRotation(new Vector(lm[16]), Vector.lerp(new Vector(lm[18]), new Vector(lm[20]), 0.5)),
     };
-    UpperArm.r.y = Vector.angleBetween3DCoords(lm[12], lm[11], lm[13]);
-    UpperArm.l.y = Vector.angleBetween3DCoords(lm[11], lm[12], lm[14]);
+
+    const UpperArm = {
+        r: new Vector({
+            x: rightLowerArmSphericalCoords.theta + 0.5,
+            y: -rightUpperArmSphericalCoords.theta,
+            z: -rightUpperArmSphericalCoords.phi,
+        }),
+        l: new Vector({
+            x: -leftLowerArmSphericalCoords.theta + 0.5,
+            y: -leftUpperArmSphericalCoords.theta,
+            z: leftUpperArmSphericalCoords.phi,
+        }),
+    };
 
     const LowerArm = {
-        r: Vector.findRotation(lm[13], lm[15]),
-        l: Vector.findRotation(lm[14], lm[16]),
-    };
-    LowerArm.r.y = Vector.angleBetween3DCoords(lm[11], lm[13], lm[15]);
-    LowerArm.l.y = Vector.angleBetween3DCoords(lm[12], lm[14], lm[16]);
-    LowerArm.r.z = clamp(LowerArm.r.z, -2.14, 0);
-    LowerArm.l.z = clamp(LowerArm.l.z, -2.14, 0);
-    const Hand = {
-        r: Vector.findRotation(
-            Vector.fromArray(lm[15]),
-            Vector.lerp(Vector.fromArray(lm[17]), Vector.fromArray(lm[19]), 0.5)
-        ),
-        l: Vector.findRotation(
-            Vector.fromArray(lm[16]),
-            Vector.lerp(Vector.fromArray(lm[18]), Vector.fromArray(lm[20]), 0.5)
-        ),
+        r: new Vector({
+            x: 0,
+            y: -rightLowerArmSphericalCoords.phi,
+            z: 0,
+        }),
+        l: new Vector({
+            x: 0,
+            y: leftLowerArmSphericalCoords.phi,
+            z: 0,
+        }),
     };
 
     //Modify Rotations slightly for more natural movement
@@ -74,28 +102,27 @@ export const rigArm = (UpperArm: Vector, LowerArm: Vector, Hand: Vector, side: S
     // Invert modifier based on left vs right side
     const invert = side === RIGHT ? 1 : -1;
 
-    UpperArm.z *= -2.3 * invert;
-    //Modify UpperArm rotationY  by LowerArm X and Z rotations
-    UpperArm.y *= PI * invert;
-    UpperArm.y -= Math.max(LowerArm.x);
-    UpperArm.y -= -invert * Math.max(LowerArm.z, 0);
-    UpperArm.x -= 0.3 * invert;
+    const rigedUpperArm = new Euler({
+        x: UpperArm.x * PI,
+        y: UpperArm.y * PI,
+        z: UpperArm.z * PI,
+        rotationOrder: "YZX",
+    });
 
-    LowerArm.z *= -2.14 * invert;
-    LowerArm.y *= 2.14 * invert;
-    LowerArm.x *= 2.14 * invert;
-
-    //Clamp values to human limits
-    UpperArm.x = clamp(UpperArm.x, -0.5, PI);
-    LowerArm.x = clamp(LowerArm.x, -0.3, 0.3);
+    const rigedLowerArm = new Euler({
+        x: LowerArm.x * PI,
+        y: LowerArm.y * PI,
+        z: LowerArm.z * PI,
+        rotationOrder: "YZX",
+    });
 
     Hand.y = clamp(Hand.z * 2, -0.6, 0.6); //side to side
     Hand.z = Hand.z * -2.3 * invert; //up down
 
     return {
         //Returns Values in Radians for direct 3D usage
-        UpperArm: UpperArm,
-        LowerArm: LowerArm,
+        UpperArm: rigedUpperArm,
+        LowerArm: rigedLowerArm,
         Hand: Hand,
     };
 };
